97df521accaa9b84a29ff94ea4bf823f9c4529e4
Renny <rennyemmanuel8@gmail.com>
2024-01-26 09:53:29 -1200

    worked on about page

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
@@ -8,5 +7,5 @@ def home(request):
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
 
