cdf66a34e6729d530c8ddd4d05b0c908e52b0304
Renny <rennyemmanuel8@gmail.com>
2024-01-26 10:30:08 -1200

    added: dummy posts

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 
 
+posts = [
+    {
+        'author': 'RennyCodes',
+        'title': 'Blog Post 1',
+        'Content': 'First post content',
+        'date_posted': 'January 27, 2024'
+    },
+    {
+        'author': 'RennyAI',
+        'title': 'Blog Post 2',
+        'Content': 'Second post content',
+        'date_posted': 'January 28, 2024'
+    }
+]
+
+
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
