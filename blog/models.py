9bdcee1895c27e040f272b9666bf0bd6938a89b1
Renny <rennyemmanuel8@gmail.com>
2024-01-28 08:04:37 -1200

    fixed spacings

--- blog/models.py
+++ blog/models.py
@@ -9,4 +9,3 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    
