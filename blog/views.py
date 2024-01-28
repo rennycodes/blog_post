2fe793f07bd75359da19dce4eb946c0d261c2053
Renny <rennyemmanuel8@gmail.com>
2024-01-28 08:44:24 -1200

    removed list of dictionaries of posts

--- blog/views.py
+++ blog/views.py
@@ -2,22 +2,6 @@ from django.shortcuts import render
 from .models import Post
 
 
-posts = [
-    {
-        'author': 'RennyCodes',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'January 27, 2024'
-    },
-    {
-        'author': 'RennyAI',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'January 28, 2024'
-    }
-]
-
-
 # Create your views here.
 def home(request):
     context = {
